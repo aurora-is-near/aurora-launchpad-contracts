f638b2f08dcabb3c71e6d7cd31b5f57080b023a1
Evgeny Ukhanov <evgeny@aurora.dev>
2025-06-18 20:11:02 +0200

    Launchpad: extend types & fix all errors related to Contract definitions

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,69 +1,123 @@
 use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{U64, U128};
-use near_sdk::store::LookupMap;
-use near_sdk::{AccountId, PanicOnDefault, PromiseOrValue, env, near, require};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::store::{LazyOption, LookupMap};
+use near_sdk::{AccountId, NearSchema, PanicOnDefault, PromiseOrValue, env, near, require};
 
-mod launch_token;
-
-#[derive(Debug, Eq, PartialEq, Clone, BorshDeserialize, BorshSerialize)]
+#[derive(
+    NearSchema,
+    Debug,
+    Eq,
+    PartialEq,
+    Clone,
+    BorshDeserialize,
+    BorshSerialize,
+    Serialize,
+    Deserialize,
+)]
+#[abi(borsh, json)]
 #[borsh(crate = "near_sdk::borsh")]
+#[serde(crate = "near_sdk::serde")]
+pub struct LaunchpadConfig {
+    pub deposit_token_account_id: AccountId,
+    pub start_date: U64,
+    pub end_date: U64,
+    pub soft_cap: U128,
+    pub mechanics: Mechanics,
+    pub vesting_schedule: Option<VestingSchedule>,
+    pub distribution_proportions: DistributionProportions,
+}
 
+#[derive(
+    NearSchema,
+    Debug,
+    Eq,
+    PartialEq,
+    Clone,
+    BorshDeserialize,
+    BorshSerialize,
+    Serialize,
+    Deserialize,
+)]
+#[abi(borsh, json)]
+#[borsh(crate = "near_sdk::borsh")]
+#[serde(crate = "near_sdk::serde")]
 pub enum Mechanics {
     FixedPrice,
 }
 
-#[derive(Debug, Eq, PartialEq, Clone, BorshDeserialize, BorshSerialize)]
+#[derive(
+    NearSchema,
+    Debug,
+    Eq,
+    PartialEq,
+    Clone,
+    BorshDeserialize,
+    BorshSerialize,
+    Serialize,
+    Deserialize,
+)]
+#[abi(borsh, json)]
 #[borsh(crate = "near_sdk::borsh")]
+#[serde(crate = "near_sdk::serde")]
 pub enum DistributionProportions {
     FixedPrice,
 }
 
-#[derive(Debug, Eq, PartialEq, Clone, BorshDeserialize, BorshSerialize)]
+#[derive(
+    NearSchema,
+    Debug,
+    Eq,
+    PartialEq,
+    Clone,
+    BorshDeserialize,
+    BorshSerialize,
+    Serialize,
+    Deserialize,
+)]
+#[abi(borsh, json)]
 #[borsh(crate = "near_sdk::borsh")]
+#[serde(crate = "near_sdk::serde")]
 pub enum VestingSchedule {
     Scheme1,
 }
 
+#[derive(
+    NearSchema,
+    Debug,
+    Ord,
+    PartialOrd,
+    Eq,
+    PartialEq,
+    Clone,
+    BorshDeserialize,
+    BorshSerialize,
+    Serialize,
+    Deserialize,
+)]
+#[abi(borsh, json)]
+#[borsh(crate = "near_sdk::borsh")]
+#[serde(crate = "near_sdk::serde")]
+pub struct IntentAccount(String);
+
 #[derive(PanicOnDefault)]
 #[near(contract_state)]
 pub struct AuroraLaunchpadContract {
-    pub token_account_id: AccountId,
-    pub deposit_token_account_id: AccountId,
-    pub start_date: u64,
-    pub end_date: u64,
-    pub soft_cap: u128,
-    pub mechanics: Mechanics,
-    pub vesting_schedule: Option<VestingSchedule>,
-    pub distribution_proportions: DistributionProportions,
+    pub config: LaunchpadConfig,
+    pub token_account_id: LazyOption<AccountId>,
     pub participants_count: u64,
     pub total_deposited: u128,
-    pub investments: LookupMap<AccountId, u128>,
+    pub investments: LookupMap<IntentAccount, u128>,
 }
 
 #[near]
 impl AuroraLaunchpadContract {
     #[init]
     #[must_use]
-    #[allow(clippy::use_self)]
-    pub fn new(
-        token_account_id: AccountId,
-        deposit_token_account_id: AccountId,
-        start_date: U64,
-        end_date: U64,
-        soft_cap: U128,
-    ) -> Self {
+    pub fn new(config: LaunchpadConfig) -> Self {
         Self {
-            token_account_id,
-            deposit_token_account_id,
-            start_date: start_date.0,
-            end_date: end_date.0,
-            soft_cap: soft_cap.0,
-            // TODO: fix after launchpad mechanics are implemented
-            mechanics: Mechanics::FixedPrice,
-            // TODO: fix after launchpad vesting schedule is implemented
-            vesting_schedule: None,
-            // TODO: fix after launchpad distribution proportions are implemented
-            distribution_proportions: DistributionProportions::FixedPrice,
+            config,
+            token_account_id: LazyOption::new(b"token_account_id".to_vec(), None),
             participants_count: 0,
             total_deposited: 0,
             investments: LookupMap::new(b"investments".to_vec()),
@@ -71,31 +125,29 @@ impl AuroraLaunchpadContract {
     }
 
     pub fn is_launchpad_leave(&self) -> bool {
-        env::block_timestamp() >= self.start_date && env::block_timestamp() < self.end_date
+        env::block_timestamp() >= self.config.start_date.0
+            && env::block_timestamp() < self.config.end_date.0
     }
 
     pub fn is_success(&self) -> bool {
-        env::block_timestamp() >= self.end_date && self.total_deposited >= self.soft_cap
+        env::block_timestamp() >= self.config.end_date.0
+            && self.total_deposited >= self.config.soft_cap.0
     }
 
     #[allow(clippy::needless_pass_by_value)]
     pub fn ft_on_transfer(
         &mut self,
-        sender_id: AccountId,
+        _sender_id: AccountId,
         amount: U128,
         msg: String,
     ) -> PromiseOrValue<U128> {
-        require!(
-            env::predecessor_account_id() == self.token_account_id,
-            "Incorrect token account id"
-        );
-
-        let _ = msg;
+        // Get Intent account from the message
+        require!(!msg.is_empty(), "Invalid transfer token message format");
+        let account = IntentAccount(msg.to_string());
 
         self.investments
-            .entry(sender_id)
+            .entry(account)
             .and_modify(|x| *x += amount.0);
-
         self.total_deposited += amount.0;
 
         PromiseOrValue::Value(0.into())
