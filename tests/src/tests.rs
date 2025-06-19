7902606640a37e79c93522ffbc9c1fb7bb7a7f41
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-06-19 17:39:54 +0100

    chore: make clippy happy

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -8,10 +8,10 @@ async fn test_create_launchpads() {
     let env = create_env().await.unwrap();
     let launchpad_config = LaunchpadConfig {
         token: LaunchpadToken {
-            total_supply: Default::default(),
-            name: "".to_string(),
-            symbol: "".to_string(),
-            icon: "".to_string(),
+            total_supply: 1_000_000.into(),
+            name: String::new(),
+            symbol: String::new(),
+            icon: String::new(),
         },
         deposit_token_account_id: env.token.id().clone(),
         start_date: 0,
@@ -19,7 +19,7 @@ async fn test_create_launchpads() {
         soft_cap: 3000.into(),
         mechanics: Mechanics::FixedPrice { price: 1.into() },
         sale_amount: None,
-        solver_allocation: Default::default(),
+        solver_allocation: 1000.into(),
         vesting_schedule: None,
         distribution_proportions: DistributionProportions::FixedPrice,
     };
