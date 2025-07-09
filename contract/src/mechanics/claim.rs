d3a99e9cbc273aa5fc66a081549536451ebaa9a5
Evgeny Ukhanov <evgeny@aurora.dev>
2025-07-10 00:05:16 +0200

    Add vesting to claim calculations and extend tests (#31)
    Co-authored-by: Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>

--- contract/src/mechanics/claim.rs
+++ contract/src/mechanics/claim.rs
@@ -10,13 +10,7 @@ pub fn available_for_claim(
     config: &LaunchpadConfig,
     timestamp: u64,
 ) -> Result<u128, &'static str> {
-    if let Some(vesting) = &config.vesting_schedule {
-        if vesting.cliff_period > timestamp {
-            return Ok(0);
-        }
-        todo!("Implement vesting schedule logic for P2 phase");
-    }
-    match config.mechanics {
+    let total_assets = match config.mechanics {
         Mechanics::FixedPrice { .. } => Ok(investment.weight),
         Mechanics::PriceDiscovery => {
             if investment.weight == 0 || total_sold_tokens == 0 {
@@ -29,16 +23,33 @@ pub fn available_for_claim(
                 .map(|result| result / U256::from(total_sold_tokens))
                 .and_then(to_u128)
         }
+    }?;
+    if let Some(vesting) = &config.vesting_schedule {
+        let vesting_start = config.end_date;
+        if timestamp < vesting_start + vesting.cliff_period {
+            return Ok(0);
+        } else if timestamp >= vesting_start + vesting.vesting_period {
+            return Ok(total_assets);
+        }
+        let elapsed = timestamp - vesting_start;
+
+        U256::from(total_assets)
+            .checked_mul(U256::from(elapsed))
+            .ok_or("Multiplication overflow")
+            .map(|result| result / U256::from(vesting.vesting_period))
+            .and_then(to_u128)
+    } else {
+        Ok(total_assets)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use aurora_launchpad_types::InvestmentAmount;
-    use near_sdk::json_types::U128;
-
     use crate::mechanics::claim::available_for_claim;
     use crate::tests::utils::{NOW, price_discovery_config};
+    use aurora_launchpad_types::InvestmentAmount;
+    use aurora_launchpad_types::config::VestingSchedule;
+    use near_sdk::json_types::U128;
 
     #[test]
     fn test_zero_weight() {
@@ -155,6 +166,130 @@ mod tests {
         let total_sold_tokens = 92_000_000;
 
         let res = available_for_claim(&investment, total_sold_tokens, &config, NOW).unwrap();
+        let expected = 173_913;
+        let expected_calc = 80_000_000 * config.sale_amount.0 / total_sold_tokens;
+        assert_eq!(res, expected);
+        assert_eq!(res, expected_calc);
+    }
+
+    #[test]
+    fn test_vesting_schedule_inside_cliff_period() {
+        let mut config = price_discovery_config();
+        let vesting_period = 2_000_000;
+        config.vesting_schedule = Some(VestingSchedule {
+            cliff_period: 500_000,
+            vesting_period,
+        });
+        let investment = InvestmentAmount {
+            amount: 80_000,
+            weight: 80_000_000,
+            claimed: 0,
+        };
+        let total_sold_tokens = 92_000_000;
+        let current_timestamp = config.end_date + 100_000; // Before cliff period ends
+
+        let res = available_for_claim(&investment, total_sold_tokens, &config, current_timestamp)
+            .unwrap();
+        let expected = 0;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_vesting_schedule_exactly_after_cliff_period() {
+        let mut config = price_discovery_config();
+        config.sale_amount = 200_000.into();
+        let vesting_period = 2_000_000;
+        config.vesting_schedule = Some(VestingSchedule {
+            cliff_period: 500_000,
+            vesting_period,
+        });
+        let investment = InvestmentAmount {
+            amount: 80_000,
+            weight: 80_000_000,
+            claimed: 0,
+        };
+        let total_sold_tokens = 92_000_000;
+        let current_timestamp = config.end_date + 500_000;
+
+        let res = available_for_claim(&investment, total_sold_tokens, &config, current_timestamp)
+            .unwrap();
+        let expected = 43478; // 80_000 * 500_000 / 2_000_000
+        let expected_calc = (80_000_000 * config.sale_amount.0 / total_sold_tokens)
+            * (u128::from(current_timestamp - config.end_date))
+            / u128::from(vesting_period);
+        assert_eq!(res, expected);
+        assert_eq!(res, expected_calc);
+    }
+
+    #[test]
+    fn test_vesting_schedule_exactly_halfway_through_vesting_period() {
+        let mut config = price_discovery_config();
+        config.sale_amount = 200_000.into();
+        let vesting_period = 2_000_000;
+        config.vesting_schedule = Some(VestingSchedule {
+            cliff_period: 500_000,
+            vesting_period,
+        });
+        let investment = InvestmentAmount {
+            amount: 80_000,
+            weight: 80_000_000,
+            claimed: 0,
+        };
+        let total_sold_tokens = 92_000_000;
+        let current_timestamp = config.end_date + vesting_period / 2; // Halfway through vesting period
+
+        let res = available_for_claim(&investment, total_sold_tokens, &config, current_timestamp)
+            .unwrap();
+        let expected = 86956;
+        let expected_calc = (80_000_000 * config.sale_amount.0 / total_sold_tokens)
+            * (u128::from(current_timestamp - config.end_date))
+            / u128::from(vesting_period);
+        assert_eq!(res, expected);
+        assert_eq!(res, expected_calc);
+    }
+
+    #[test]
+    fn test_vesting_schedule_exactly_at_vesting_period_end() {
+        let mut config = price_discovery_config();
+        config.sale_amount = 200_000.into();
+        let vesting_period = 2_000_000;
+        config.vesting_schedule = Some(VestingSchedule {
+            cliff_period: 500_000,
+            vesting_period,
+        });
+        let investment = InvestmentAmount {
+            amount: 80_000,
+            weight: 80_000_000,
+            claimed: 0,
+        };
+        let total_sold_tokens = 92_000_000;
+        let current_timestamp = config.end_date + 2_000_000; // Exactly at the end of vesting period
+
+        let res = available_for_claim(&investment, total_sold_tokens, &config, current_timestamp)
+            .unwrap();
+        let expected = 173_913;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_vesting_schedule_after_vesting_period_end() {
+        let mut config = price_discovery_config();
+        config.sale_amount = 200_000.into();
+        let vesting_period = 2_000_000;
+        config.vesting_schedule = Some(VestingSchedule {
+            cliff_period: 500_000,
+            vesting_period,
+        });
+        let investment = InvestmentAmount {
+            amount: 80_000,
+            weight: 80_000_000,
+            claimed: 0,
+        };
+        let total_sold_tokens = 92_000_000;
+        let current_timestamp = config.end_date + 2_000_000; // Exactly at the end of vesting period
+
+        let res = available_for_claim(&investment, total_sold_tokens, &config, current_timestamp)
+            .unwrap();
         let expected = 173_913;
         assert_eq!(res, expected);
     }
