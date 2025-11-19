d190b365bf77d3d74ec99453773db5e4d12cb507
Evgeny Ukhanov <evgeny@aurora.dev>
2025-11-19 18:16:08 +0100

    Updates releas.yml release process

---
+++ scripts/gen-launchpad-types.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+
+existing_json="./target/near/aurora_launchpad_contract/aurora_launchpad_contract_abi.json"
+type_output="./res/schema.ts"
+
+# shellcheck disable=SC2016
+jq_filter='
+# Extract JSON Schema
+.body.root_schema
+
+# Get rid of unnecessary "type"
+| del(.type)
+
+# Set reasonable title
+| .title = "Aurora Launchpad Contract ABI"
+
+# Problem: WebAuthn definition includes "properties" and "onOf" together.
+#          Tools cannot resolve such ambiguity. So we separate them into
+#          two different definitions.
+| .definitions.MultiPayload.oneOf |= (
+      map(
+        if ((.properties.standard.enum // []) | contains(["webauthn"])) then
+          . as $webauthn |
+          $webauthn.anyOf | map(
+            {
+              type: "object",
+              description: .description,
+              required: ($webauthn.required + .required),
+              properties: ($webauthn.properties + .properties)
+            }
+          )
+        else
+          [.]
+        end
+      ) | flatten
+    )
+'
+
+# Ensure the output directory exists
+mkdir -p "$(dirname "$type_output")"
+
+# Step 1: Check if the JSON file exists; if not, download and extract it in memory
+if [ ! -f "$existing_json" ]; then
+  echo "JSON file not found. Downloading and processing artifact in memory..."
+
+else
+  echo "JSON file found. Processing existing file..."
+
+  # Apply jq filter to the existing file
+  schema=$(jq "$jq_filter" "$existing_json")
+fi
+
+# Step 2: Pass the modified JSON directly to json-schema-to-typescript
+echo "$schema" | npm_config_registry="https://registry.npmjs.org" npx json-schema-to-typescript -o "$type_output" --unreachableDefinitions
+# Prettify the output
+# pnpm biome format "$type_output" --write
+
+echo "Types generated successfully in "$type_output""
+
